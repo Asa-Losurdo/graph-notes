29fe106c83cfa6419c806d42ebbd1851cb690d47
Asa-Losurdo <asa.losurdo@gmail.com>
2023-05-02 21:17:33 -0400

    pack can now find the wasm files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::process::Command;
+use glob::glob;
 
 fn main() {
+
     //pass through --release flag
     let mut cmd = Command::new("cargo");
     cmd.args(["build", "-p", "graph-notes", "--target", "wasm32-wasi"]);
@@ -11,5 +13,21 @@ fn main() {
     cmd.status()
     .expect("Failed to execute command");
 
-    // encode wasm
+
+
+    // get wasm files
+    let mut pattern = "./target/wasm32-wasi/".to_owned();
+    #[cfg(debug)]{
+       pattern = pattern + "debug"; 
+    }
+    #[cfg(release)]{
+        pattern = pattern + "release";
+    }
+    pattern = pattern + "/*.wasm";
+    let paths = glob(&pattern).expect("Failed to read glob pattern");
+
+    for e in paths {
+        println!("{}", e.unwrap().display());
+    }
+
 }
