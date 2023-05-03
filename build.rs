8bb34fb3899c1bf63fe17be0552862a501e1e4ea
Asa-Losurdo <asa.losurdo@gmail.com>
2023-05-02 20:37:14 -0400

    fixed version number

--- build.rs
+++ build.rs
@@ -7,6 +7,6 @@ pub fn main() {
     // https://doc.rust-lang.org/cargo/reference/build-scripts.html#rustc-cfg
     // https://users.rust-lang.org/t/conditional-compilation-for-debug-release/1098/7
     // #[cfg(debug)] or #[cfg(release)] 
-    let profile = env::var("PROFILE").unwrap();
+    let profile = env::var("PROFILE").expect("pack's build.rs failed to get env var 'PROFILE'");
     println!("cargo:rustc-cfg={profile}");
 }
